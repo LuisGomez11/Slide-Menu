a50746690876729b88bf25fd238372a3d0260c61
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-09-18 00:02:59 -0500

    Slide Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $ : any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontend';
+
+  ngOnInit(){
+	$(".icono-bar").click(function () {
+          $(".menu").toggleClass('menu-activado');
+    });
+
+  }
 }
